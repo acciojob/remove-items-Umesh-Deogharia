8e8abcddaf8e093073ae831bbdc74898c932e0d0
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-10-14 20:34:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let select = document.getElementById('colorSelect');
+let btn = document.querySelector('input');
+let option = document.querySelectorAll('option');
+
+let arr = [];
+
+let selectOpt = [...select.options];
+
+btn.addEventListener("click",check);
+function check(e){
+    let selectedValue = select.value;
+    select.innerHTML = "";
+    arr = selectOpt.filter((elem)=> selectedValue !== elem.value);
+   arr.forEach((option) => select.appendChild(option));
+    // let filtered = selectOpt.filter((elem)=>{
+    //     return elem.value !== check2
+    // } );
+    selectOpt = arr;
+}
+
+
+
+
